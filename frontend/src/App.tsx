cd80c76677d4a265e5605c6e49e11dbb7c7d8cae
baha-ott <bahaalout126@gmail.com>
2023-09-06 12:07:29 +0300

    Move routing, providers to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { apiSlice } from "./api/apiSlice";
+import { store } from "./app/store";
+import { router } from "./router/routers";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ApiProvider api={apiSlice}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </ApiProvider>
+    </>
+  );
 }
 
 export default App;
